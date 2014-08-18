c7c62cc9380da5f8b1bf717f6fb797bd649acb80
Heny Hollar <henyhollar@gmail.com>
2014-08-19 01:32:45 +0200

    queue message_as_sms

--- Tibaty_Arooko/message/tasks.py
+++ Tibaty_Arooko/message/tasks.py
@@ -4,3 +4,6 @@ from .views import message_as_sms, message_as_email
 @task(retries=3, retry_delay=120)
 def online_sms(data):
     message_as_sms(data)
+
+def online_email(data):
+    message_as_email()
