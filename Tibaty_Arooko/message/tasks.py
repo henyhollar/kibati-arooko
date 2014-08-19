9f4fa9037cbb154944fba61ba38f48e57a99bfd5
Heny Hollar <henyhollar@gmail.com>
2014-08-19 02:49:37 +0200

    task_sms

--- Tibaty_Arooko/message/tasks.py
+++ Tibaty_Arooko/message/tasks.py
@@ -2,8 +2,9 @@ from huey.djhuey import task, periodic_task, crontab
 from .views import message_as_sms, message_as_email
 
 @task(retries=2, retry_delay=120)
-def online_sms(data):
+def task_sms(data):
     message_as_sms(data)
 
-def online_email(data):
+
+def task_email(data):
     message_as_email(data)
